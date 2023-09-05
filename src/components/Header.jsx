14cc259a5b74d681a063dd8538ec8ca430240a39
Pratama89 <pratama.adiciptawan@gmail.com>
2023-09-05 08:32:45 +0700

    ada error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
 /* eslint-disable react/prop-types */
 export default function Header({ author }) {
-    return <h1>Belajar React bareng {author ? author : 'WPU'} 🚀</h1>;
+    return 
+    <div className="">
+      <h1>Belajar React bareng {author ? author : 'WPU'} 🚀</h1>;
+    </div>
   }
   
