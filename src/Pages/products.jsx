b1ed2f0963cfbe3e6c53fd5fe88c160ab7de5851
Pratama89 <pratama.adiciptawan@gmail.com>
2023-09-12 08:42:35 +0700

    memperbaiki Page Card

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,27 +1,28 @@
 import Button from "../components/Elements/Button";
+import CardProduct from "../components/Fragments/CardProduct";
 
 const ProductsPage = () => {
   return (
-    <div className="border text-center w-64 h-full mx-auto mt-2 rounded-md shadow-md ">
-      <div className="mt-3">
-        <h1 className="font-bold">Produk</h1>
-        <div className="max-w-sm  mt-5 mx-2 mb-5 ">
-            <a href="https://www.tokopedia.com/pharmadent/dental-lab-vacuum-former-forming-molding-machine-equipment">
-            <img src="/img/Vacuum.png" alt="product" className="rounded-md bg-blue-300" />
-            </a>
-            <div className="mt-2 text-sm">
-              <h2 className="text-xl font-semibold">Vacuum</h2>
-              <p className="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni natus cupiditate earum eum, tenetur laudantium.</p>
-            </div>
-            <div className="flex items-center justify-center">
-              <span className="text-3xl font-bold ">1.825.000</span>
-            </div>
-              <Button ClassName="bg-blue-500 hover:bg-blue-600" >Masuk ke Keranjang</Button>
-            </div>
-        </div>
-    </div>
+    // <div className="border text-center w-64 h-full mx-auto mt-2 rounded-md shadow-md ">
+    //   <div className="mt-3">
+    //     <h1 className="font-bold">Produk</h1>
+    //     <div className="max-w-sm  mt-5 mx-2 mb-5 ">
+    //         <a href="https://www.tokopedia.com/pharmadent/dental-lab-vacuum-former-forming-molding-machine-equipment">
+    //         <img src="/img/Vacuum.png" alt="product" className="rounded-md bg-blue-300" />
+    //         </a>
+    //         <div className="mt-2 text-sm">
+    //           <h2 className="text-xl font-semibold">Vacuum</h2>
+    //           <p className="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni natus cupiditate earum eum, tenetur laudantium.</p>
+    //         </div>
+    //         <div className="flex items-center justify-center">
+    //           <span className="text-3xl font-bold ">1.825.000</span>
+    //         </div>
+    //           <Button ClassName="bg-blue-500 hover:bg-blue-600" >Masuk ke Keranjang</Button>
+    //         </div>
+    //     </div>
+    // </div>
     // return (
-    // <CardProduct>
+    <CardProduct />
     //     <CardProduct.Header></CardProduct.Header>
     //     <CardProduct.Body></CardProduct.Body>
     //     <CardProduct.Footer></CardProduct.Footer>
--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -3,45 +3,48 @@ import Button from "../Elements/Button";
 const CardProduct = () => {
     return (
       <div className="border text-center w-64 h-full mx-auto mt-2 rounded-md shadow-md ">
+      
+        <Header />
+        <Body />
+        <Footer />
+            
+    </div>
+    );
+};
 
-      </div>
-    )
-}
 
 const Header = () => {
   return (
     <div className="mt-3">
-      <h1 className="font-bold">Produk</h1>
-      <div className="max-w-sm  mt-5 mx-2 mb-5 ">
-        <a href="https://www.tokopedia.com/pharmadent/dental-lab-vacuum-former-forming-molding-machine-equipment">
-          <img src="/img/Vacuum.png" alt="product" className="rounded-md bg-blue-300" />
-        </a>      
+        <h1 className="font-bold">Produk</h1>
+        <div className="max-w-sm  mt-5 mx-2 mb-5 ">
+            <a href="https://www.tokopedia.com/pharmadent/dental-lab-vacuum-former-forming-molding-machine-equipment">
+            <img src="/img/Vacuum.png" alt="product" className="rounded-md bg-blue-300" />
+            </a>
+          </div>
       </div>
-    </div>
   )
 }
 
 const Body = () => {
   return (
-    <div className="mt-2 text-sm">
+    <div className="mt-2 text-sm px-2">
       <h2 className="text-xl font-semibold">Vacuum</h2>
-        <p className="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni natus cupiditate earum eum, tenetur laudantium.</p>
+      <p className="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni natus cupiditate earum eum, tenetur laudantium.</p>
     </div>
   )
 }
 
 const Footer = () => {
-  return (    
+  return (
+    <>
     <div className="flex items-center justify-center">
       <span className="text-3xl font-bold ">1.825.000</span>
-    <Button ClassName="bg-blue-500 hover:bg-blue-600">Masukkan ke Keranjang</Button>    
     </div>
-  )
+      <Button ClassName="bg-blue-500 hover:bg-blue-600 mb-4" >Masuk ke Keranjang</Button>
+    </>
+    )
 }
-
-CardProduct.Header = Header;
-CardProduct.Body = Body;
-CardProduct.Footer = Footer;
 
 
 export default CardProduct;
